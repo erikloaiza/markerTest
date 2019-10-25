063fe4214d1fbce04439b0c004067159ede4b021
ErikLoaiza <erik.loaiza@uao.edu.co>
2019-10-25 13:51:01 -0500

    updated

--- js/applyStereo3.js
+++ js/applyStereo3.js
@@ -59,8 +59,8 @@ AFRAME.registerComponent("ar-stereo", {
       vrDisplay.deviceInfo_.distortion.coefficients[0] = 0.07;
       vrDisplay.deviceInfo_.distortion.coefficients[0] = 0.03;
       vrDisplay.deviceInfo_.viewer.baselineLensDistance = 0.028;
-      vrDisplay.deviceInfo_.viewer.screenLensDistance = 0.040;
-      //vrDisplay.deviceInfo_.viewer.screenLensDistance = 0.080;
+      //vrDisplay.deviceInfo_.viewer.screenLensDistance = 0.040;
+      vrDisplay.deviceInfo_.viewer.screenLensDistance = 0.080;
       vrDisplay.deviceInfo_.viewer.fov = 30;
       vrDisplay.deviceInfo_.viewer.interLensDistance = 0.05;
       vrDisplay.deviceInfo_.viewer.distortionCoefficients = [0.07, 0.03];
